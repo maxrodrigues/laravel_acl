82d4fae15536bf8dd2dfb0c7fc8df34e7ef0785f
Max Rodrigues <maxuel.rodrigues@gmail.com>
2020-09-30 18:04:51 -0300

    add roles and permissions

---
+++ database/migrations/2020_09_30_205546_create_permission_roles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->integer('role_id')->unsigned();
+            $table->integer('permission_id')->unsigned();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_roles');
+    }
+}
